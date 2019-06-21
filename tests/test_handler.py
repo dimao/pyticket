b6f63d99e56467be37b908289eb102a759d4c6ab
Dmitriy Ovsianikov <dimao@intes.by>
2019-06-21 14:21:48 +0300

    Add wsdl test

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,7 +1,10 @@
 import json
 
 import arrow
+from zeep import Transport, Settings
+
 from pyticket import handler
+from pyticket import import_ticket
 
 with open('config.json', 'r') as f:
     config = json.load(f)
@@ -12,3 +15,11 @@ def test_scrape():
     next_game_date_string = html_string[0].strip('<p>\t')
     next_game_datetime = arrow.get(next_game_date_string, 'DD MMMM YYYY, HH:mm', locale='ru')
     assert type(next_game_datetime) is arrow.arrow.Arrow
+
+
+def test_conn():
+    wsdl = config['wsdl']['url']
+    transport = Transport(timeout=10)
+    settings = Settings(strict=False)  # because of invalid xml response when calling UpdateEventSession
+    it = import_ticket.ImportTicket(wsdl, transport, settings)
+    assert it.client.service.GetCurrentVersion() == '1.0.1.0'
